6a041532baee645f5e6585e0cd819aab3ad2e3f0
Nur Zahirah Zamri <zahirah.zamri@live.iium.edu.my>
2020-08-15 10:16:36 +0800

    Update DataService.js

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,1 +1,29 @@
+import { db } from '../config/db';
+import { Actions } from 'react-native-router-flux';
 
+export const AddSubscriptionApp =  (subsAppName,amount,renewalPeriod,paymentMethod,reminder,date) => {
+    db.ref('/apps').child(subsAppName).set({
+        subsAppName: subsAppName,
+        amount: amount,
+        renewalPeriod: renewalPeriod,
+        paymentMethod: paymentMethod,
+        reminder: reminder,
+        date: date,
+    }, () => Actions.ListScreen());
+}
+
+export const updateSubscriptionApp =  (subsAppName,amount,renewalPeriod,paymentMethod,reminder,date) => {
+    db.ref('/apps').child(subsAppName).update({
+        subsAppName: subsAppName,
+        amount: amount,
+        renewalPeriod: renewalPeriod,
+        paymentMethod: paymentMethod,
+        reminder: reminder,
+        date: date
+    }, () => Actions.ListScreen());
+}
+
+export const removeSubscriptionApp =  (subsAppName) => {
+    db.ref('/apps').child(subsAppName).remove();
+    Actions.ListScreen();
+}
