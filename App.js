7c306fe4fe8eb894df5e56655576dd346d25eda7
Nur Zahirah Zamri <nurzahirahz20@gmail.com>
2020-08-15 10:14:59 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+import Tab1 from './src/screens/Tab1';
+import ListScreen from './src/screens/ListScreen';
+import AddSubscriptions from './src/screens/AddSubscriptions';
+import ViewSubscription from './src/screens/ViewSubscription';
+import UpdateSubscription from './src/screens/UpdateSubscription';
+import MainScreen from './src/screens/MainScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router headerMode = 'none'>
+        <Scene key="root">
+          <Scene key="LoginScreen" component={LoginScreen} left={()=>null} title="Notify_U"  />
+          <Scene key="SignupScreen" component={SignupScreen} left={()=>null} title="Mureed" />
+          
+          <Scene key="MainScreen" component={MainScreen} left={()=>null} title="Notify_U" initial={true}/>
+          <Scene key="ListScreen" component={ListScreen} left={()=>null} title="Notify_U" />
+          <Scene key="Tab1" component={Tab1} left={()=>null} title="Notify_U"/>
+          
+          <Scene key="AddSubscriptions" component={AddSubscriptions} left={()=>null} title="Notify_U" />
+          <Scene key="ViewSubscription" component={ViewSubscription} left={()=>null} title="Notify_U"/>
+          <Scene key="UpdateSubscription" component={UpdateSubscription} left={()=>null} title="Notify_U"/>
+        </Scene>
+      </Router>
+    )
+  }
+}
