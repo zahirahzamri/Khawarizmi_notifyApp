7580351c1bda13200795d9abe410d5eda002f20f
Nur Izyan <izyan.rosli@live.iium.edu.my>
2020-08-15 12:01:48 +0800

    Update DataService.js

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,35 +1,35 @@
 import { db } from '../config/db';
 import { Actions } from 'react-native-router-flux';
 
-export const AddSubscriptionApp =  (subsAppName,amount,renewalPeriod,paymentMethod,reminder,date) => {
+export const AddSubscriptionApp =  (subsAppName,amount,renewalPeriod,paymentMethod,reminder) => {
     db.ref('/apps').child(subsAppName).set({
         subsAppName: subsAppName,
         amount: amount,
         renewalPeriod: renewalPeriod,
         paymentMethod: paymentMethod,
         reminder: reminder,
-        date: date,
+        date: new Date().toString(),
     }, () => Actions.ListScreen());
 }
 
-export const updateSubscriptionApp =  (subsAppName,amount,renewalPeriod,paymentMethod,reminder,date) => {
+export const updateSubscriptionApp =  (subsAppName,amount,renewalPeriod,paymentMethod,reminder) => {
     db.ref('/apps').child(subsAppName).update({
         subsAppName: subsAppName,
         amount: amount,
         renewalPeriod: renewalPeriod,
         paymentMethod: paymentMethod,
         reminder: reminder,
-        date: date
+        date: new Date().toString()
     }, () => Actions.ListScreen());
 }
 
-export const AddUtilitiesApp =  (utilitiesName,renewalPeriod,paymentMethod,reminder,date) => {
+export const AddUtilitiesApp =  (utilitiesName,renewalPeriod,paymentMethod,reminder) => {
     db.ref('/apps').child(utilitiesName).set({
         utilitiesName: utilitiesName,
         renewalPeriod: renewalPeriod,
         paymentMethod: paymentMethod,
         reminder: reminder,
-        date: date,
+        date: new Date().toString(),
     }, () => Actions.ListScreen());
 }
 
