4223b71541d16b6e61da6461d8053a9b2861d7ac
Nur Zahirah Zamri <zahirah.zamri@live.iium.edu.my>
2020-08-15 14:53:37 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { Router, Scene } from 'react-native-router-flux';
 import ListScreen from './src/screens/ListScreen';
 import Tab1 from './src/screens/Tab1';
 import AddSubscriptions from './src/screens/AddSubscriptions';
-import ViewSubscription from './src/screens/ViewSubscription';
 import UpdateSubscription from './src/screens/UpdateSubscription';
 import MainScreen from './src/screens/MainScreen';
 import LoginScreen from './src/screens/LoginScreen';
@@ -25,17 +24,16 @@ export default class App extends Component {
           
           <Scene key="MainScreen" component={MainScreen} left={()=>null} title="Notify_U" initial={true}/>
           <Scene key="ListScreen" component={ListScreen} left={()=>null} title="Notify_U" />
+          
           <Scene key="Tab1" component={Tab1} left={()=>null} title="Notify_U" />
-            
+          <Scene key="AddSubscriptions" component={AddSubscriptions} left={()=>null} title="Notify_U" />
+          <Scene key="UpdateSubscription" component={UpdateSubscription} left={()=>null} title="Notify_U"/>
             
           <Scene key="AddUtilitiies" component={AddUtilities} left={()=>null} title="Notify_U" />
           <Scene key="ViewUtilities" component={ViewUtilities} left={()=>null} title="Notify_U" />
           <Scene key="UpdateUtilities" component={UpdateUtilities} left={()=>null} title="Notify_U" />
           <Scene key="UtilitiesListScreen" component={UtilitiesListScreen} left={()=>null} title="Notify_U" />
-          
-          <Scene key="AddSubscriptions" component={AddSubscriptions} left={()=>null} title="Notify_U" />
-          <Scene key="ViewSubscription" component={ViewSubscription} left={()=>null} title="Notify_U"/>
-          <Scene key="UpdateSubscription" component={UpdateSubscription} left={()=>null} title="Notify_U"/>
+
         </Scene>
       </Router>
     )
