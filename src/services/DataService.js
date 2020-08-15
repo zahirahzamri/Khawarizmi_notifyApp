33e82d823764efd083ec65185b19defc5219559b
Nur-ain2 <61967993+Nur-ain2@users.noreply.github.com>
2020-08-15 11:44:49 +0800

    Add files via upload

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -27,3 +27,28 @@ export const removeSubscriptionApp =  (subsAppName) => {
     db.ref('/apps').child(subsAppName).remove();
     Actions.ListScreen();
 }
+
+export const AddUtilitiesApp =  (utilitiesName,duePeriod,paymentMethod,reminder,date) => {
+    db.ref('/apps').child(utilitiesName).set({
+        utilitiesName: utilitiesName,
+        duePeriod: duePeriod,
+        paymentMethod: paymentMethod,
+        reminder: reminder,
+        date: date,
+    }, () => Actions.AddUtilities());
+}
+
+export const updateUtilitiesApp =  (utilitiesName,duePeriod,paymentMethod,reminder,date) => {
+    db.ref('/apps').child(utilitiesName).update({
+        utilitiesName: utilitiesName,
+        duePeriod: duePeriod,
+        paymentMethod: paymentMethod,
+        reminder: reminder,
+        date: date
+    }, () => Actions.AddUtilities());
+}
+
+export const removeUtilities =  (utilitiesName) => {
+    db.ref('/apps').child(utilitiesName).remove();
+    Actions.AddUtilities();
+}
